92db2fae17c854a28039872b22580ec85b5a2618
Mohan <mohan3@9diamonds.llnl.gov>
2019-07-26 18:46:46 -0700

    Incremented 3rd level version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='pysaber',
-      version='0.1.2',
+      version='0.1.3',
       description='Python package that implements a systems approach to blur estimation and reduction (SABER)',
       long_description=long_description,
       long_description_content_type="text/markdown",
